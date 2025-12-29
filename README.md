55d6f343c6c0f33295197b19a7d664832c54b182
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2025-12-29 15:12:19 -0500

    feat: bootstrap file

--- README.md
+++ README.md
@@ -9,13 +9,11 @@ Collection of role for configuring home server.
 2. Run following commands in `bash`:
 
   ```bash
-  ansible-galaxy collection install --upgrade community.general
-
   export ANSIBLE_CALLBACK_RESULT_FORMAT=yaml  # yaml output format instead of json
   export ANSIBLE_VERBOSITY=1  # Set verbosity, default is 0
 
   ansible-galaxy collection install \
-      "git+https://github.com/ChunPanYung/ansi_colle-server.git"
+      "git+https://github.com/ChunPanYung/ansi_colle-server.git,main"
 
   # Run this to update every time
   ansible-playbook ansi_colle.server.install
---
+++ bootstraph.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Install ansible & Enable ssh on RedHat and Debian based system
+function setup-debian {
+	export DEBIAN_FRONTEND=noninteractive
+	apt install --yes openssh-server
+	systemctl enable --now ssh
+	command -v ufw && ufw allow ssh
+
+	apt install --yes ansible-core
+}
+
+function setup-redhat {
+	dnf install --assumeyes openssh-server
+	systemctl enable --now sshd
+	if command -v firewall-cmd &>/dev/null; then
+		firewall-cmd --permanent --add-service=ssh
+		firewall-cmd --reload
+	fi
+
+	dnf install --assumeyes python3-psutil # for community.general.dconf
+	dnf install --assumeyes ansible-core
+}
+
+# Check sudo -- if not sudo, ask user to become sudo
+if [[ "$EUID" != 0 ]]; then
+	echo "Execute this program with 'sudo'!"
+	exit 1
+fi
+
+os_name="$(grep -e '^NAME=' /etc/os-release | cut -d '=' -f2 | tr -d '\"')"
+# execute following depending on OS
+case "${os_name,,}" in
+*"debian"*)
+	echo "Debian"
+	setup-debian
+	;;
+*"fedora"* | *"almalinux"* | *"red hat"* | *"rockylinux"*)
+	echo "RedHat family OS"
+	setup-redhat
+	;;
+esac
+
+# Install ansible collection
+ansible-galaxy collection install --upgrade community.general
